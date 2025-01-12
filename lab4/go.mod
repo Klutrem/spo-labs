7a59f3ece3c06d4c028b824e44c2e6bf2f728fa5
Klutrem <danyaryabov@mail.ru>
2025-01-12 23:57:09 +0300

    working parser with memory calculation

--- lab4/go.mod
+++ lab4/go.mod
@@ -4,7 +4,16 @@ go 1.23.4
 
 require (
 	github.com/alecthomas/participle/v2 v2.1.1
-	github.com/alecthomas/repr v0.2.0
+	github.com/alecthomas/repr v0.4.0
 )
 
-require github.com/alecthomas/participle v0.7.1 // indirect
+require (
+	github.com/pointlander/compress v1.1.1-0.20190518213731-ff44bd196cc3 // indirect
+	github.com/pointlander/jetset v1.0.1-0.20190518214125-eee7eff80bd4 // indirect
+	github.com/pointlander/peg v1.0.1 // indirect
+)
+
+require (
+	github.com/alecthomas/kong v1.6.1
+	github.com/alecthomas/participle v0.7.1 // indirect
+)
--- lab4/main.go
+++ lab4/main.go
@@ -2,156 +2,138 @@ package main
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/alecthomas/participle/v2"
 	"github.com/alecthomas/participle/v2/lexer"
 )
 
-// Структуры для грамматики
-
 type Program struct {
-	Types []*Type `@@*`
+	Pos lexer.Position
+
+	Types []*TypeDeclaration `@@*`
+	Vars  []*VarDeclaration  `@@*`
 }
 
-type Type struct {
-	Name   string   `"type" @Ident`
-	Fields []*Field `@@*`
+type TypeDeclaration struct {
+	Pos lexer.Position
+
+	TypeKeyword string   `"type"`
+	Name        string   `@Ident`
+	Fields      []*Field `"{" @@* "}"`
+}
+
+type VarDeclaration struct {
+	Pos lexer.Position
+
+	VarKeyword string `"var"`
+	Name       string `@Ident`
+	Type       string `"=" @Ident ";"`
 }
 
 type Field struct {
-	VarName string `@Ident "="`
-	Expr    Expr   `@@`
+	Pos lexer.Position
+
+	Name string `@Ident`
+	Type string `":" @("byte" | "real") ";"`
 }
 
-type Expr interface {
-	Size(alignment int) int
-}
+var (
+	lex = lexer.MustSimple([]lexer.SimpleRule{
+		{"comment", `//.*|/\*.*?\*/`},
+		{"whitespace", `\s+`},
 
-type ScalarType struct {
-	Type string `@("byte" | "real")`
-}
+		{"TypeKeyword", `\btype\b`},
+		{"VarKeyword", `\bvar\b`},
+		{"Ident", `[a-zA-Z_][a-zA-Z0-9_]*`},
+		{"Punct", `[-,(){}=;:]`},
+		{"Literal", `\b(byte|real)\b`},
+	})
+	parser = participle.MustBuild[Program](
+		participle.Lexer(lex),
+		participle.UseLookahead(2),
+	)
+)
 
-func (s ScalarType) Size(alignment int) int {
-	size := 0
-	switch s.Type {
-	case "byte":
-		size = 1
-	case "real":
-		size = 6
-	}
+const (
+	ByteSize  = 1 // Размер типа byte в байтах
+	RealSize  = 6 // Размер типа real в байтах
+	Alignment = 2 // Кратность распределения
+)
 
-	// Выравнивание
-	if size%alignment != 0 {
-		padding := alignment - (size % alignment)
-		size += padding
-	}
-	return size
-}
-
-type Union struct {
-	Fields []*UnionField `"union" @@* "end"`
-}
-
-type UnionField struct {
-	VarName string `@Ident ":"`
-	Type    string `@("byte" | "real")`
-}
-
-func (u Union) Size(alignment int) int {
+func calculateTypeSize(typ *TypeDeclaration) int {
 	totalSize := 0
-	for _, field := range u.Fields {
-		var fieldSize int
+	for _, field := range typ.Fields {
 		switch field.Type {
 		case "byte":
-			fieldSize = 1
+			totalSize += ByteSize
 		case "real":
-			fieldSize = 6
+			totalSize += RealSize
 		}
-
-		// Выравнивание
-		if fieldSize%alignment != 0 {
-			padding := alignment - (fieldSize % alignment)
-			fieldSize += padding
-		}
-
-		totalSize += fieldSize
+	}
+	// Выравнивание до кратности
+	if totalSize%Alignment != 0 {
+		totalSize += Alignment - (totalSize % Alignment)
 	}
 	return totalSize
 }
 
+func calculateVariableSize(varDecl *VarDeclaration, types map[string]int) int {
+	size, exists := types[varDecl.Type]
+	if !exists {
+		panic(fmt.Sprintf("Unknown type: %s", varDecl.Type))
+	}
+	return size
+}
+
 func main() {
-	// Лексер для грамматики
-	lexer := lexer.MustSimple([]lexer.SimpleRule{
-		{`Ident`, `[a-zA-Z][a-zA-Z_\d]*`},
-		{`Type`, `byte|real`},
-		{`Union`, `union`},
-		{`End`, `end`},
-		{`Punct`, `[,]`},
-		{`Whitespace`, `\s+`},
-		{`Semicolon`, `;`}, // Точка с запятой
-	})
+	const sample = `
+type MyType {
+    field1 : byte;
+    field2 : real;
+}
 
-	// Парсер с использованием библиотеки participle
-	parser := participle.MustBuild[Program](
-		participle.Lexer(lexer),
-		participle.Union[Expr](ScalarType{}, Union{}),
-	)
+type AnotherType {
+    field3 : real;
+    field4 : byte;
+}
 
-	// Пример входного текста
-	input := `
-type MyType
-  var1 = byte;
-  var2 = real;
-  var3 = union
-    var4: byte;
-    var5: real;
-  end
-
-type AnotherType
-  var6 = byte;
+var var1 = MyType;
+var var2 = AnotherType;
+var var3 = byte;
+var var4 = real;
 `
+
+	// Парсинг входного текста
+	ast, err := parser.ParseString("", sample)
+	if err != nil {
+		panic(err)
+	}
 
 	// Вывод исходного текста
 	fmt.Println("Исходный текст:")
-	fmt.Println(input)
+	fmt.Println(sample)
 
-	// Парсинг строки
-	program, err := parser.ParseString("", input)
-	if err != nil {
-		log.Fatalf("error: %s", err.Error())
+	// Расчет размеров типов
+	types := map[string]int{
+		"byte": ByteSize,
+		"real": RealSize,
 	}
-
-	// Задаем кратность выравнивания (2 байта)
-	alignment := 2
-
-	// Вывод размеров типов данных
 	fmt.Println("\nРазмеры типов данных:")
-	for _, t := range program.Types {
-		fmt.Printf("Тип '%s':\n", t.Name)
-		for _, field := range t.Fields {
-			size := field.Expr.Size(alignment)
-			fmt.Printf("  Переменная '%s': %d байт (с учетом выравнивания)\n", field.VarName, size)
-		}
+	for _, typ := range ast.Types {
+		size := calculateTypeSize(typ)
+		types[typ.Name] = size
+		fmt.Printf("Тип %s: %d байт\n", typ.Name, size)
 	}
 
-	// Вывод размеров объединений (если есть)
-	for _, t := range program.Types {
-		for _, field := range t.Fields {
-			if union, ok := field.Expr.(Union); ok {
-				fmt.Printf("  Объединение '%s': %d байт (с учетом выравнивания)\n", field.VarName, union.Size(alignment))
-			}
-		}
+	// Расчет размеров переменных
+	totalMemory := 0
+	fmt.Println("\nРазмеры переменных:")
+	for _, varDecl := range ast.Vars {
+		size := calculateVariableSize(varDecl, types)
+		fmt.Printf("Переменная %s: %d байт\n", varDecl.Name, size)
+		totalMemory += size
 	}
 
-	// Вычисление итогового объема памяти
-	var totalSize int
-	for _, t := range program.Types {
-		for _, field := range t.Fields {
-			totalSize += field.Expr.Size(alignment)
-		}
-	}
-
-	// Итоговый объем памяти
-	fmt.Printf("\nИтоговый объем памяти: %d байт (с учетом выравнивания)\n", totalSize)
+	// Вывод суммарного объема памяти
+	fmt.Printf("\nСуммарный объем памяти: %d байт\n", totalMemory)
 }
